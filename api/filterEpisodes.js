5ec54f151ca6273bd65e7210afe4262013dca731
jenniferrepreza2024 <jenniferrepreza2024>
2023-11-20 09:24:05 -0600

    Edit filter in app .js

--- api/filterEpisodes.js
+++ api/filterEpisodes.js
@@ -3,10 +3,10 @@
 import axios from 'axios';
 import baseUrl from '../utils/baseUrl.js'; // Ajusté la ruta para que sea relativa
 
-async function filterEpisodes(name, status) {
+async function filterEpisodes(name , status) {
   try {
     const response = await axios.get(`${baseUrl}/episode`, {
-      params: { name, status },
+      params: { name , status},
     });
     return response.data.results;
   } catch (error) {
